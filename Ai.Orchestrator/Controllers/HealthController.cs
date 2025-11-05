eb945969b086f85321c25950adad072ebb77268e
Jitan Gupta <gtangupta@gmail.com>
2025-11-05 10:54:04 +0530

    added basic ai api project

---
+++ Ai.Orchestrator/Controllers/HealthController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ai.Orchestrator.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HealthController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Service is running.");
+        }
+    }
+}
